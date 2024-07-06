2e2f1a93f510026d4605f63be455d0fb5f00db64
MellGriffin <missmelissagriffin@gmail.com>
2024-07-06 17:22:14 -0500

    Lots of nice code, nothing happening

--- app.js
+++ app.js
@@ -3,42 +3,67 @@ const calculator = document.querySelector('#calculator');
 const buttons = document.querySelectorAll('.button');
 const display = document.querySelector('.display');
 
+const numButton = document.querySelectorAll('.number');//all numbers
+const operatorButton = document.querySelectorAll('.operator');//all operators
+const equalButton = document.querySelectorAll('.equals');//equal button
+const clearButton = document.querySelector('button[textContent="C"]');//clear button
+
 /*-------------------------------- Variables --------------------------------*/
+let num1 = null;//first number pressed
+let num2 = null;//second number pressed
+let op = null;//current operator
+let result = null;//result of equation
+let currentOperator = null;//current operator picked
 
 /*------------------------ Cached Element References ------------------------*/
 //classes: button number, row, display, button operator, button equals
+
 /*----------------------------- Event Listeners -----------------------------*/
 
 calculator.addEventListener('click', (event) => {
-  display.textContent = event.target.innerText;
-
-  // console.log(event.target.innerText); //How to store each click on the display line?
-
-if(event.target.classList.contains('number')) {
-
-  console.log(convertNumber);
-  // console.dir(event.target.value);
-  //Do something with a number, convert string to number
-}
-
-if(event.target.innerText === '*' || '+' || '-' || '=' || '/') {
-
-  // console.dir(event.target.value);//Do something with this operator, convert string
-}
-
-if(event.target.innerText === 'C') {
-
-  // console.dir(event.target.value);//Do something with this operator, convert string
-}
-
-});
-
+  const clickedButton = event.target;
+  display.textContent = clickedButton.innerText;
+  if (clickedButton.classList.contains('number')) { //number buttons
+    if (currentOperator === null) {
+      num1 = num1 === null ? clickedButton.innerText : num1 + clickedButton.innerText;
+      display.textContent = num1;
+    } else {
+      num2 = num2 === null ? clickedButton.innerText : num2 + clickedButton.innerText;
+      display.textContent = num2;
+    }
+  }
+})
+  if (clickedButton === equalButton) {
+    if (num1 !== null && num2 !== null && currentOperator !== null) { //if two numbers have been selected WITH an operator
+      num1 = parseFloat(num1);
+      num2 = parseFloat(num2);
+      switch (currentOperator) {
+        case '+':
+          result = num1 + num2;
+          break;
+        case '-':
+          result = num1 - num2;
+          break;
+        case '*':
+          result = num1 * num2;
+          break;
+        case '/':
+          result = num1 / num2;
+          break;
+      }
+      display.textContent = result;
+      num1 = result.toString(); //store result as num1
+      num2 = null;
+      currentOperator = null;
+    }
+    clearButton.addEventListener('click', (event) => {
+      num1 = null;
+      num2 = null;
+      currentOperator = null;
+      result = null;
+      display.textContent = '0'; //return everything to zero
+    });
+  }
 
 /*-------------------------------- Functions --------------------------------*/
-
-
-
-const add = () => {}
-const sub = () => {}
-const mult = () => {}
-const divide = () => {}
+//
